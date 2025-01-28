a14b51ab5cdd4338e9f3b48f6adc54c0a986dfb3
siwakasen <siwakasen@gmail.com>
2025-01-28 21:36:14 +0700

    rm funding

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -2,6 +2,6 @@ export const siteConfig = {
   title: 'Ride Bali Explore',
   description:
     'High-quality tour guide in Bali that will take you to see the beautiful world.',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
+  /** Without additional '/' on the end, e.g.  */
   url: 'https://tour.cashtrack.my.id',
 };
